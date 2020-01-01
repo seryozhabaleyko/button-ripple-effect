0533285eb8aa12e0769e5baa8a38e066884211a4
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-01 21:20:06 +0300

    Create ripple.js

---
+++ javascript/ripple.js
@@ -0,0 +1,30 @@
+const rippleElement = document.querySelectorAll('.ripple-effect')
+
+function rippleEffect(e) {
+    let target = e.target
+    console.log(target)
+
+    let rect = target.getBoundingClientRect()
+    console.log(rect)
+
+    let ripple = target.querySelector('.ripple')
+    console.log(ripple)
+
+    if (!ripple) {
+        ripple = document.createElement('span')
+        ripple.className = 'ripple'
+        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
+        this.appendChild(ripple)
+    }
+    ripple.classList.remove('show')
+    let top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop
+    let left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft
+    ripple.style.top = top + 'px'
+    ripple.style.left = left + 'px'
+    ripple.classList.add('show')
+    return false
+}
+
+rippleElement.forEach(ripple => {
+    ripple.addEventListener('click', rippleEffect, false)
+})
