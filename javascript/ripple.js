424ca9eceedb0dd8dfe2230382bd5d09b20434f5
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-26 00:10:38 +0300

    Update ripple.js

--- javascript/ripple.js
+++ javascript/ripple.js
@@ -1,36 +1,36 @@
-const $ = el => document.querySelectorAll(el)
+const $ = el => document.querySelectorAll(el);
 
 $('.btn').forEach(el => {
     el.addEventListener('mousedown', function(e) {
 
-        let ripple = document.createElement('span')
-        ripple.className = 'btn-ripple'
-        this.appendChild(ripple)
+        let ripple = document.createElement('span');
+        ripple.className = 'btn-ripple';
+        this.appendChild(ripple);
 
-        let rect = this.getBoundingClientRect()
+        let rect = this.getBoundingClientRect();
 
-        let width = rect.width
-        let height = rect.height
+        let width = rect.width;
+        let height = rect.height;
 
-        let posX = e.pageX - rect.left
-        let posY = e.pageY - rect.top
+        let posX = e.pageX - rect.left;
+        let posY = e.pageY - rect.top;
 
-        let rippleStyle = new RippleStyle(width, height, posX, posY)
+        let rippleStyle = new RippleStyle(width, height, posX, posY);
 
-        ripple.style.width = `${rippleStyle.width}px`
-        ripple.style.height = `${rippleStyle.height}px`
-        ripple.style.top = `${rippleStyle.top}px`
-        ripple.style.left = `${rippleStyle.left}px`
+        ripple.style.width = `${rippleStyle.width}px`;
+        ripple.style.height = `${rippleStyle.height}px`;
+        ripple.style.top = `${rippleStyle.top}px`;
+        ripple.style.left = `${rippleStyle.left}px`;
 
         ripple.addEventListener('animationend', function () {
-            this.remove()
+            this.remove();
         })
     })
 })
 
 function RippleStyle(width, height, posX, posY) {
-    this.width = (width <= height) ? height : width
-    this.height = (width <= height) ? height : width
-    this.top = posY - (this.height * 0.5)
-    this.left = posX - (this.width * 0.5)
+    this.width = (width <= height) ? height : width;
+    this.height = (width <= height) ? height : width;
+    this.top = posY - (this.height * 0.5);
+    this.left = posX - (this.width * 0.5);
 }
