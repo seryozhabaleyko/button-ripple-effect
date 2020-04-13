81471e95cf84a04be7135fe233038c5a363db287
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-04-13 20:30:00 +0300

    Update ripple.js

--- javascript/ripple.js
+++ javascript/ripple.js
@@ -1,36 +1,38 @@
-const $ = el => document.querySelectorAll(el);
-
-$('.btn').forEach(el => {
-    el.addEventListener('mousedown', function(e) {
-
-        let ripple = document.createElement('span');
-        ripple.className = 'btn-ripple';
-        this.appendChild(ripple);
-
-        let rect = this.getBoundingClientRect();
-
-        let width = rect.width;
-        let height = rect.height;
-
-        let posX = e.pageX - rect.left;
-        let posY = e.pageY - rect.top;
-
-        let rippleStyle = new RippleStyle(width, height, posX, posY);
-
-        ripple.style.width = `${rippleStyle.width}px`;
-        ripple.style.height = `${rippleStyle.height}px`;
-        ripple.style.top = `${rippleStyle.top}px`;
-        ripple.style.left = `${rippleStyle.left}px`;
-
-        ripple.addEventListener('animationend', function () {
-            this.remove();
-        })
-    })
-})
-
-function RippleStyle(width, height, posX, posY) {
-    this.width = (width <= height) ? height : width;
-    this.height = (width <= height) ? height : width;
-    this.top = posY - (this.height * 0.5);
-    this.left = posX - (this.width * 0.5);
+class RippleStyle {
+    constructor(width, height, posX, posY) {
+        this.width = (width <= height) ? height : width;
+        this.height = (width <= height) ? height : width;
+        this.top = posY - (this.height * 0.5);
+        this.left = posX - (this.width * 0.5);
+    }
 }
+
+function rippleHandler(e) {
+    const $span = document.createElement('span');
+    $span.className = 'btn-ripple';
+    this.append($span);
+
+    const {
+        width, height, left, top,
+    } = this.getBoundingClientRect();
+
+    const posX = e.pageX - left;
+    const posY = e.pageY - top;
+
+    const rippleStyle = new RippleStyle(width, height, posX, posY);
+
+    $span.style.width = `${rippleStyle.width}px`;
+    $span.style.height = `${rippleStyle.height}px`;
+    $span.style.top = `${rippleStyle.top}px`;
+    $span.style.left = `${rippleStyle.left}px`;
+
+    $span.addEventListener('animationend', () => {
+        $span.remove();
+    }, false);
+}
+
+const ripple = (element) => {
+    element.addEventListener('mousedown', rippleHandler, false);
+};
+
+export default ripple;
